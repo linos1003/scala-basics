d3954aedacc1d0dd0d8b524ae015622af906cbec
linos1003 <smida.bassem@gmail.com>
2017-08-14 16:49:16 +0200

    init scala basic examples

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by bsmida on 14/08/17.
+  */
+object Main extends App {
+
+  val numbers = List(1, 2, 11, 9, 100, 90, 33, 67)
+  val result = numbers.fold(0) { (x, y) => x + y }
+  print(result)
+}
